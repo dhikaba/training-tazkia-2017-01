92a6bbda7938166a06ab42e3bdcdfa35d28cc32a
Endy Muhardin <endy.muhardin@gmail.com>
2017-03-21 15:02:22 +0700

    memasang layout untuk header/footer

--- belajar-springboot/src/main/java/id/ac/tazkia/training/controller/SupirController.java
+++ belajar-springboot/src/main/java/id/ac/tazkia/training/controller/SupirController.java
@@ -3,6 +3,7 @@ package id.ac.tazkia.training.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Date;
@@ -18,5 +19,16 @@ public class SupirController {
         mm.addAttribute("cari", nama);
         return mm;
 
+    }
+
+    @RequestMapping(value = "/supir/form", method = RequestMethod.GET)
+    public void tampilkanForm(){
+
+    }
+
+
+    @RequestMapping(value = "/supir/form", method = RequestMethod.POST)
+    public String prosesForm(){
+        return "redirect:/supir/list";
     }
 }
