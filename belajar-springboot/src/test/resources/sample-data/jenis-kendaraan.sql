152034ca132b46f8a03a3c7547ad4888575197d4
Endy Muhardin <endy.muhardin@gmail.com>
2017-03-23 16:03:05 +0700

    sample data test

---
+++ belajar-springboot/src/test/resources/sample-data/jenis-kendaraan.sql
@@ -0,0 +1,8 @@
+insert into jenis_kendaraan (nama, keterangan)
+VALUES ('Toyota', 'Rush');
+
+insert into jenis_kendaraan (nama, keterangan)
+VALUES ('Daihatsu', 'Grand Max');
+
+insert into jenis_kendaraan (nama, keterangan)
+VALUES ('Suzuki', 'APV');
