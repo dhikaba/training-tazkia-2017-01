f6ee2e3e577ba77ac7b964336eeea6e5bc418179
Endy Muhardin <endy.muhardin@gmail.com>
2017-03-23 16:58:48 +0700

    display form kendaraan

--- belajar-springboot/src/main/java/id/ac/tazkia/training/controller/JenisKendaraanController.java
+++ belajar-springboot/src/main/java/id/ac/tazkia/training/controller/JenisKendaraanController.java
@@ -1,10 +1,13 @@
 package id.ac.tazkia.training.controller;
 
 import id.ac.tazkia.training.dao.JenisKendaraanDao;
+import id.ac.tazkia.training.entity.JenisKendaraan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class JenisKendaraanController {
@@ -15,5 +18,14 @@ public class JenisKendaraanController {
     @RequestMapping("/jeniskendaraan/list")
     public ModelMap daftarKendaraan(){
         return new ModelMap("daftarJenisKendaraan", jkDao.findAll());
+    }
+
+    @RequestMapping(value = "/jeniskendaraan/form", method = RequestMethod.GET)
+    public ModelMap tampilkanForm(@RequestParam(value = "id", required = false) JenisKendaraan jk){
+        if(jk == null){
+            jk = new JenisKendaraan();
+        }
+
+        return new ModelMap("jenisKendaraan", jk);
     }
 }
