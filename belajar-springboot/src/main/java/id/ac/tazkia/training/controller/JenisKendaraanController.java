4c1f57a2d0625df14934508f5617586432832413
Endy Muhardin <endy.muhardin@gmail.com>
2017-03-23 16:45:05 +0700

    display data dari database

--- belajar-springboot/src/main/java/id/ac/tazkia/training/controller/JenisKendaraanController.java
+++ belajar-springboot/src/main/java/id/ac/tazkia/training/controller/JenisKendaraanController.java
@@ -1,5 +1,7 @@
 package id.ac.tazkia.training.controller;
 
+import id.ac.tazkia.training.dao.JenisKendaraanDao;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -7,8 +9,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class JenisKendaraanController {
 
+    @Autowired
+    private JenisKendaraanDao jkDao;
+
     @RequestMapping("/jeniskendaraan/list")
     public ModelMap daftarKendaraan(){
-        return new ModelMap();
+        return new ModelMap("daftarJenisKendaraan", jkDao.findAll());
     }
 }
---
+++ belajar-springboot/src/main/java/id/ac/tazkia/training/dao/JenisKendaraanDao.java
@@ -0,0 +1,7 @@
+package id.ac.tazkia.training.dao;
+
+import id.ac.tazkia.training.entity.JenisKendaraan;
+import org.springframework.data.repository.PagingAndSortingRepository;
+
+public interface JenisKendaraanDao extends PagingAndSortingRepository<JenisKendaraan, Integer> {
+}
