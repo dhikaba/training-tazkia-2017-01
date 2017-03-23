0773ece2a22d96a4ebc13ea8f7f169ae5bb9e5a2
Endy Muhardin <endy.muhardin@gmail.com>
2017-03-23 17:07:31 +0700

    implement save dan update

--- belajar-springboot/src/main/java/id/ac/tazkia/training/controller/JenisKendaraanController.java
+++ belajar-springboot/src/main/java/id/ac/tazkia/training/controller/JenisKendaraanController.java
@@ -5,9 +5,14 @@ import id.ac.tazkia.training.entity.JenisKendaraan;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.support.SessionStatus;
+
+import javax.validation.Valid;
 
 @Controller
 public class JenisKendaraanController {
@@ -27,5 +32,15 @@ public class JenisKendaraanController {
         }
 
         return new ModelMap("jenisKendaraan", jk);
+    }
+
+    @RequestMapping(value = "/jeniskendaraan/form", method = RequestMethod.POST)
+    public String prosesForm(@ModelAttribute @Valid JenisKendaraan jk, BindingResult err, SessionStatus status){
+        if(err.hasErrors()){
+            return "/jeniskendaraan/form";
+        }
+        jkDao.save(jk);
+        status.setComplete();
+        return "redirect:/jeniskendaraan/list";
     }
 }
